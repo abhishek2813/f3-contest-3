f8e125ceb108fb59524f56a4216748cc2502d465
abhishek2813 <abhi93158879@gmail.com>
2023-02-25 21:40:34 +0530

    update cuurent user

--- login.js
+++ login.js
@@ -1,46 +1,45 @@
-var currentitem = JSON.parse(localStorage.getItem("currentUser"));// getting surent Item Obj
-if(currentitem){
-  window.location.href = "dashboard.html";
+var currentitem = JSON.parse(localStorage.getItem("currentUser")); // getting surent Item Obj
+if (currentitem) { // if current user exit send to dashboard page
+    window.location.href = "dashboard.html";
 }
 const login = document.getElementById('login-form');
-    const errorloginMsg = document.getElementById('error-msg');
-    login.addEventListener('submit', (e) => {
-      e.preventDefault();
-      
-      const email = login.elements['email'].value.trim();
-      const password = login.elements['password'].value;
+const errorloginMsg = document.getElementById('error-msg');
+login.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-      
+    const email = login.elements['email'].value.trim();
+    const password = login.elements['password'].value;
 
-      let formData = JSON.parse(localStorage.getItem('users')) || [];
-           // Check if the entered email and password match any of the users in the array
-     const currentUser = formData.find(user => user.email === email && user.password === password);
-     if (currentUser) {
+
+
+    let formData = JSON.parse(localStorage.getItem('users')) || [];
+    // Check if the entered email and password match any of the users in the array
+    const currentUser = formData.find(user => user.email === email && user.password === password);
+    if (currentUser) {
         // Create a current user object with email, password, name, and token
         const currentUserObj = {
-          email: currentUser.email,
-          name: currentUser.name,
-         // password: currentUser.password,
-          token: generateToken(),
+            email: currentUser.email,
+            name: currentUser.name,
+            token: generateToken(),
         };
         // Store the current user object in local storage
         localStorage.setItem('currentUser', JSON.stringify(currentUserObj));
         // Redirect the user to the dashboard page
         window.location.href = 'dashboard.html';
-      } else {
+    } else {
         // Display an error message to the user
         errorloginMsg.textContent = 'Invalid email or password. Please try again.';
-      }
-        
-    });
-   
-    
-    function generateToken() {
-      // A simple function to generate a random 16 character string
-      const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-      let token = '';
-      for (let i = 0; i < 16; i++) {
-        token += charset.charAt(Math.floor(Math.random() * charset.length));
-      }
-      return token;
     }
+
+});
+
+
+function generateToken() {
+    // A simple function to generate a random 16 character string
+    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let token = '';
+    for (let i = 0; i < 16; i++) {
+        token += charset.charAt(Math.floor(Math.random() * charset.length));
+    }
+    return token;
+}
